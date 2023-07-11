4242bcd5eb4bede5b761bd1b96c32073d05085e5
Shukrullojon <shukrullobk@gmail.com>
2023-07-11 12:19:24 +0500

    Comment api.php route:list

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,27 @@
 <?php
 
+// auth
 Route::group(['prefix' => 'auth', 'namespace' => '\App\Http\Controllers\Api'], function () {
     Route::post('/register', 'AuthController@register');
     Route::post('/login', 'AuthController@login');
 });
 
+// day
 Route::group(['prefix' => 'day', 'namespace' => '\App\Http\Controllers\Api'], function () {
     Route::get('/get', 'DayController@get');
 });
 
+// module
 Route::group(['prefix' => 'module', 'namespace' => '\App\Http\Controllers\Api'], function () {
     Route::get('/get', 'ModuleController@get');
 });
 
+// teacher
 Route::group(['prefix' => 'teacher', 'namespace' => '\App\Http\Controllers\Api'], function () {
     Route::get('/get', 'TeacherController@get');
 });
 
+// info
 Route::group(['prefix' => 'info', 'namespace' => '\App\Http\Controllers\Api'], function () {
     Route::get('/get', 'InfoController@get');
 });
